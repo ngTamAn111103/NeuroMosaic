3ed41d54e367e68c277ee7e14c9890316183c8f0
ngTamAn111103 <ng.t.ann2003@gmail.com>
2025-12-09 20:11:44 +0700

    Cấu hình auto sort class để đồng bộ code + index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        ...
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
