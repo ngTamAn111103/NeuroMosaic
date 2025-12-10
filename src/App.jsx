19d1b90ed2082d7170e80fa538f90f99c0b12357
ngTamAn111103 <ng.t.ann2003@gmail.com>
2025-12-10 10:26:30 +0700

    fix something

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import { useState } from "react";
-import UIOverlay from "./components/UIOverlay";
 
 function App() {
-  // 
   return (
     <>
-      <div className="relative h-screen w-full">
-        {/* UI Overlay */}
-        <UIOverlay/>
-      </div>
     </>
   );
 }
