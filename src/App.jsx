a3e7393773a5d0367316ca35f446f49d76b04300
ngTamAn111103 <ng.t.ann2003@gmail.com>
2025-12-10 10:20:35 +0700

    Coppy paste UIOverlay

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
+import UIOverlay from "./components/UIOverlay";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  // 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-      <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        ...
-      </button>
+      <div className="relative h-screen w-full">
+        {/* UI Overlay */}
+        <UIOverlay/>
+      </div>
     </>
   );
 }
