2a807383c8302d6838210f375944277931c2a319
ngTamAn111103 <ng.t.ann2003@gmail.com>
2025-12-11 15:56:57 +0700

    a

--- src/components/ImageItem.jsx
+++ src/components/ImageItem.jsx
@@ -1,10 +1,13 @@
 // 3D
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 import * as THREE from "three";
 // React
-import { useState } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 
 function ImageItem({ data, textureCache, setSelectedImage, config }) {
+  const meshRef = useRef(); // Điều khiển phần xác position, scale, rotation, ...
+  const materialRef = useRef(); // Điều khiển phần hồn opacity, transparent,...
+
   // Kiểm tra và lấy ra data.thumb_path từ textureCache truyền vào
   const cached = textureCache?.[data.thumb_path];
   // Nếu cached có -> Sử dụng cached
@@ -12,12 +15,57 @@ function ImageItem({ data, textureCache, setSelectedImage, config }) {
   const texture = cached || useLoader(THREE.TextureLoader, data.thumb_path);
 
   // Lấy vị trí
-  const [x, y, z] = data.position;
+  // const [x, y, z] = data.position;
+  const target = new THREE.Vector3(...data.position);
+
+  // Hàm chạy trước khi ảnh được sinh ra
+  useLayoutEffect(() => {
+    if (meshRef.current) {
+      meshRef.current.position.set(0, 0, 0); // bắt đầu từ tâm
+    }
+    if (materialRef.current) {
+      materialRef.current.opacity = 0; // ban đầu trong suốt
+      materialRef.current.transparent = true;
+    }
+  }, []);
+
+  // Chạy 60 lần/s
+  useFrame((state, delta) => {
+    if (!meshRef.current || !materialRef.current) return;
+
+    // damp(a, b, λ, dt): Làm mượt giá trị từ a → b, càng gần càng chậm (giống easing-out).
+    meshRef.current.position.x = THREE.MathUtils.damp(
+      meshRef.current.position.x,
+      target.x,
+      2, // tốc độ
+      delta,
+    );
+    meshRef.current.position.y = THREE.MathUtils.damp(
+      meshRef.current.position.y,
+      target.y,
+      2,
+      delta,
+    );
+    meshRef.current.position.z = THREE.MathUtils.damp(
+      meshRef.current.position.z,
+      target.z,
+      2,
+      delta,
+    );
+
+    // Fade in opacity
+    materialRef.current.opacity = THREE.MathUtils.damp(
+      materialRef.current.opacity,
+      1,
+      1,
+      delta,
+    );
+  });
 
   return (
     // Một mặt phẳng hiển thị ảnh
     <mesh
-      position={[x, y, z]}
+      ref={meshRef}
       onPointerDown={(e) => {
         // Chỉ click chuột trái mới hiển thị ảnh
         if (e.button !== 0) return;
@@ -29,7 +77,7 @@ function ImageItem({ data, textureCache, setSelectedImage, config }) {
       {/* Kích thước mặt phẳng */}
       <planeGeometry args={config.planeGeometry_args} />
       {/* Dán texture (ảnh) lên mặt phẳng */}
-      <meshBasicMaterial map={texture} />
+      <meshBasicMaterial ref={materialRef} map={texture} />
     </mesh>
   );
 }
